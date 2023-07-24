57a644546998dc585a1e241848a2afef8335c22d
antgrutta <antgrutta@github.com>
2023-07-24 10:56:22 -0400

    Fixed large queries

--- internal/api/api.go
+++ internal/api/api.go
@@ -3,51 +3,10 @@ package api
 import (
 	"context"
 
-	"github.com/mona-actions/gh-migrate-teams/internal/team"
 	"github.com/shurcooL/githubv4"
 	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
 )
-
-var query struct {
-	Organization struct {
-		Teams struct {
-			PageInfo struct {
-				EndCursor   githubv4.String
-				HasNextPage bool
-			}
-			Edges []struct {
-				Node struct {
-					Name    string
-					Members struct {
-						PageInfo struct {
-							EndCursor   githubv4.String
-							HasNextPage bool
-						}
-						Edges []struct {
-							Node struct {
-								Login string
-								Email string
-							}
-						}
-					} `graphql:"members(first: $first, after: $members_after)"`
-					Repositories struct {
-						PageInfo struct {
-							EndCursor   githubv4.String
-							HasNextPage bool
-						}
-						Edges []struct {
-							Permission string
-							Node       struct {
-								Name string
-							}
-						}
-					} `graphql:"repositories(first: $first, after: $repositories_after)"`
-				}
-			}
-		} `graphql:"teams(first: $first, after: $teams_after)"`
-	} `graphql:"organization(login: $login)"`
-}
 
 func newGHClient(token string) *githubv4.Client {
 	src := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
@@ -56,57 +15,148 @@ func newGHClient(token string) *githubv4.Client {
 	return githubv4.NewClient(httpClient)
 }
 
-func GetSourceOrganizationTeams() []team.Team {
+func GetSourceOrganizationTeams() []string {
 	client := newGHClient(viper.GetString("source_token"))
 
+	var query struct {
+		Organization struct {
+			Teams struct {
+				PageInfo struct {
+					EndCursor   githubv4.String
+					HasNextPage bool
+				}
+				Edges []struct {
+					Node struct {
+						Slug string
+					}
+				}
+			} `graphql:"teams(first: $first, after: $after)"`
+		} `graphql:"organization(login: $login)"`
+	}
+
 	variables := map[string]interface{}{
-		"login":              githubv4.String(viper.Get("SOURCE_ORGANIZATION").(string)),
-		"first":              githubv4.Int(100),
-		"teams_after":        (*githubv4.String)(nil),
-		"members_after":      (*githubv4.String)(nil),
-		"repositories_after": (*githubv4.String)(nil),
+		"login": githubv4.String(viper.Get("SOURCE_ORGANIZATION").(string)),
+		"first": githubv4.Int(100),
+		"after": (*githubv4.String)(nil),
 	}
 
-	err := client.Query(context.Background(), &query, variables)
-	if err != nil {
-		panic(err)
-	}
-
-	// Get all teams, team membership, and team repositories
-	var teams []team.Team
+	teams := make([]string, 0)
 	for {
-		for _, orgTeam := range query.Organization.Teams.Edges {
-			// Get all team members
-			var members []team.Member
-			for {
-				for _, member := range orgTeam.Node.Members.Edges {
-					members = append(members, team.Member{Login: member.Node.Login, Email: member.Node.Email})
-				}
-				if !orgTeam.Node.Members.PageInfo.HasNextPage {
-					break
-				}
-				variables["members_after"] = githubv4.NewString(orgTeam.Node.Members.PageInfo.EndCursor)
-			}
-			// Get all team repositories
-			var repositories []team.Repository
-			for {
-				for _, repository := range orgTeam.Node.Repositories.Edges {
-					repositories = append(repositories, team.Repository{Name: repository.Node.Name, Permission: repository.Permission})
-				}
-				if !orgTeam.Node.Repositories.PageInfo.HasNextPage {
-					break
-				}
-				variables["repositories_after"] = githubv4.NewString(orgTeam.Node.Repositories.PageInfo.EndCursor)
-			}
-			// Append to teams slice
-			teams = append(teams, team.Team{Name: orgTeam.Node.Name, Members: members, Repositories: repositories})
+		err := client.Query(context.Background(), &query, variables)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, team := range query.Organization.Teams.Edges {
+			teams = append(teams, team.Node.Slug)
 		}
 
 		if !query.Organization.Teams.PageInfo.HasNextPage {
 			break
 		}
-		variables["teams_after"] = githubv4.NewString(query.Organization.Teams.PageInfo.EndCursor)
+
+		variables["after"] = githubv4.NewString(query.Organization.Teams.PageInfo.EndCursor)
 	}
 
 	return teams
+}
+
+func GetTeamMemberships(team string) [][]string {
+	client := newGHClient(viper.GetString("source_token"))
+
+	var query struct {
+		Organization struct {
+			Team struct {
+				Members struct {
+					PageInfo struct {
+						EndCursor   githubv4.String
+						HasNextPage bool
+					}
+					Edges []struct {
+						Node struct {
+							Login string
+							Email string
+						}
+					}
+				} `graphql:"members(first: $first, after: $after)"`
+			} `graphql:"team(slug: $slug)"`
+		} `graphql:"organization(login: $login)"`
+	}
+
+	variables := map[string]interface{}{
+		"login": githubv4.String(viper.Get("SOURCE_ORGANIZATION").(string)),
+		"slug":  githubv4.String(team),
+		"first": githubv4.Int(100),
+		"after": (*githubv4.String)(nil),
+	}
+
+	var members [][]string
+	for {
+		err := client.Query(context.Background(), &query, variables)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, member := range query.Organization.Team.Members.Edges {
+			members = append(members, []string{member.Node.Login, member.Node.Email})
+		}
+
+		if !query.Organization.Team.Members.PageInfo.HasNextPage {
+			break
+		}
+
+		variables["after"] = githubv4.NewString(query.Organization.Team.Members.PageInfo.EndCursor)
+	}
+
+	return members
+}
+
+func GetTeamRepositories(team string) [][]string {
+	client := newGHClient(viper.GetString("source_token"))
+
+	var query struct {
+		Organization struct {
+			Team struct {
+				Repositories struct {
+					PageInfo struct {
+						EndCursor   githubv4.String
+						HasNextPage bool
+					}
+					Edges []struct {
+						Permission string
+						Node       struct {
+							Name string
+						}
+					}
+				} `graphql:"repositories(first: $first, after: $after)"`
+			} `graphql:"team(slug: $slug)"`
+		} `graphql:"organization(login: $login)"`
+	}
+
+	variables := map[string]interface{}{
+		"login": githubv4.String(viper.Get("SOURCE_ORGANIZATION").(string)),
+		"slug":  githubv4.String(team),
+		"first": githubv4.Int(100),
+		"after": (*githubv4.String)(nil),
+	}
+
+	var repositories [][]string
+	for {
+		err := client.Query(context.Background(), &query, variables)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, repo := range query.Organization.Team.Repositories.Edges {
+			repositories = append(repositories, []string{repo.Node.Name, repo.Permission})
+		}
+
+		if !query.Organization.Team.Repositories.PageInfo.HasNextPage {
+			break
+		}
+
+		variables["after"] = githubv4.NewString(query.Organization.Team.Repositories.PageInfo.EndCursor)
+	}
+
+	return repositories
 }
--- pkg/export/export.go
+++ pkg/export/export.go
@@ -2,31 +2,65 @@ package export
 
 import (
 	"encoding/csv"
-	"fmt"
 	"os"
 
 	"github.com/mona-actions/gh-migrate-teams/internal/api"
-	"github.com/mona-actions/gh-migrate-teams/internal/team"
+	"github.com/pterm/pterm"
 	"github.com/spf13/viper"
 )
 
 func CreateCSVs() {
-	// Get team membership
-	var teams []team.Team
-	teams = api.GetSourceOrganizationTeams()
+	// Get all teams from source organization
+	teamsSpinnerSuccess, _ := pterm.DefaultSpinner.Start("Fetching teams from organization...")
+	teams := api.GetSourceOrganizationTeams()
+	teamsSpinnerSuccess.Success()
 
-	fmt.Println("Found", len(teams), "teams")
+	// Get all team members from source organization
+	membershipsSpinnerSuccess, _ := pterm.DefaultSpinner.Start("Fetching team memberships...")
+	memberships := getMemberships(teams)
+	membershipsSpinnerSuccess.Success()
 
-	// Create Membership CSV
-	createMembershipCSV(teams)
+	// Get all team repositories from source organization
+	repositoriesSpinnerSuccess, _ := pterm.DefaultSpinner.Start("Fetching team repositories...")
+	repositories := getRepositories(teams)
+	repositoriesSpinnerSuccess.Success()
 
-	// Create Repository CSV
-	createRepositoryCSV(teams)
+	// Create team membership csv
+	createCSVMembershipsSpinnerSuccess, _ := pterm.DefaultSpinner.Start("Creating team membership csv...")
+	createCSV(memberships, viper.GetString("OUTPUT_FILE")+"-team-membership.csv")
+	createCSVMembershipsSpinnerSuccess.Success()
+
+	// Create team repository csv
+	createCSVRepositoriesSpinnerSuccess, _ := pterm.DefaultSpinner.Start("Creating team repository csv...")
+	createCSV(repositories, viper.GetString("OUTPUT_FILE")+"-team-repositories.csv")
+	createCSVRepositoriesSpinnerSuccess.Success()
 }
 
-func createMembershipCSV(teams []team.Team) {
+func getMemberships(teams []string) [][]string {
+	memberships := make([][]string, 0)
+	for _, team := range teams {
+		members := api.GetTeamMemberships(team)
+		for _, member := range members {
+			memberships = append(memberships, []string{team, member[0], member[1]})
+		}
+	}
+	return memberships
+}
+
+func getRepositories(teams []string) [][]string {
+	repositories := make([][]string, 0)
+	for _, team := range teams {
+		repos := api.GetTeamRepositories(team)
+		for _, repo := range repos {
+			repositories = append(repositories, []string{team, repo[0], repo[1]})
+		}
+	}
+	return repositories
+}
+
+func createCSV(data [][]string, filename string) {
 	// Create team membership csv
-	file, err := os.Create(viper.GetString("OUTPUT_FILE") + "-team-membership.csv")
+	file, err := os.Create(filename)
 	if err != nil {
 		panic(err)
 	}
@@ -36,30 +70,8 @@ func createMembershipCSV(teams []team.Team) {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	// Write team membership to csv
-	for _, team := range teams {
-		for _, member := range team.Members {
-			writer.Write([]string{team.Name, member.Login, member.Email})
-		}
-	}
-}
-
-func createRepositoryCSV(teams []team.Team) {
-	// Create team membership csv
-	file, err := os.Create(viper.GetString("OUTPUT_FILE") + "-team-repository-permissions.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	// Initialize csv writer
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	// Write team membership to csv
-	for _, team := range teams {
-		for _, repository := range team.Repositories {
-			writer.Write([]string{team.Name, repository.Name, repository.Permission})
-		}
+	// Write team memberships to csv
+	for _, line := range data {
+		writer.Write(line)
 	}
 }
